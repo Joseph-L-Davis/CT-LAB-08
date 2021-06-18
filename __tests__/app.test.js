1af88b4fd54a2a6f8696a06344556bce81807c9c
Joseph Davis <joethebartenderpdx@gmail.com>
2021-06-17 19:29:20 -0700

    findById func, route, test all good

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,13 +2,14 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import Pajama from '../lib/models/Pajama.js';
 
 describe('pajama routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
-  it('POST and text user', async () => {
+  it.skip('POST and text user', async () => {
     const res = await request(app)
       .post('/api/v1/pajamas')
       .send({
@@ -22,5 +23,22 @@ describe('pajama routes', () => {
       color: 'peach',
       size: 'XXL'
     });
+  });
+
+  it('GET pajama by ID', async () => {
+    const bluePajama = await Pajama.insert({
+      color: 'yellow',
+      size: 'medium'
+    });
+
+    const yellowPajama = await Pajama.insert({
+      color: 'blue',
+      size: 'small'
+    });
+
+    const res = await request(app)
+      .get(`/api/v1/pajamas/${yellowPajama.id}`);
+
+    expect(res.body).toEqual(yellowPajama);
   });
 });
