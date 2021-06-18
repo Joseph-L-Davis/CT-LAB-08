e4151b29aac87b9a01dd8cb3c1e1f16d12042db8
Joseph Davis <joethebartenderpdx@gmail.com>
2021-06-17 17:34:39 -0700

    tested sendText, insert and PUT

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,8 +3,24 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
-describe('demo routes', () => {
+describe('pajama routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  it('POST and text user', async () => {
+    const res = await request(app)
+      .post('/api/v1/pajamas')
+      .send({
+        color: 'peach',
+        size: 'XXL'
+      });
+    console.log(res.body);
+
+    expect(res.body).toEqual({
+      id: '1',
+      color: 'peach',
+      size: 'XXL'
+    });
   });
 });
